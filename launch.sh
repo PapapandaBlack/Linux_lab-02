bb3daf5060e529d0bdce30096505e703414aa904
Evgeny Kalishenko <ydginster@gmail.com>
2023-05-29 00:21:48 +0300

    Real fall point method prototype added
    Signed-off-by: Evgeny Kalishenko <ydginster@gmail.com>

---
+++ launch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+calculate_fall_point() {
+  echo "0" > fp.txt
+}
+
+calculate_accident_fall_point() {
+  echo "0" > accident_fp.txt
+}
+
+calculate_satellite_orbit() {
+  echo "0" > orbit.txt
+}
+
+calculate_flight_time() {
+  echo "0" > flight-time.txt
+}
+
+calculate_fuel() {
+  echo "0" > fuel.txt
+}
+
+calculate_fall_point
+calculate_accident_fall_point
+calculate_satellite_orbit
+calculate_flight_time
+calculate_fuel
