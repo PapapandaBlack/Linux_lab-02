32bfa49f1b8ebcbf7d984b30262d442f35fcbc20
st117094 <st117094@student.spbu.ru>
2024-05-10 10:11:31 +0100

    fix bug in simple_satellite_orbit()

--- launch.sh
+++ launch.sh
@@ -25,4 +25,32 @@ calculate_fuel() {
 calculate_accident_fall_point
 calculate_satellite_orbit
 calculate_flight_time
+calculate_fuel#!/bin/bash
+
+calculate_accident_fall_point() {
+  echo "0" > accident_fp.txt
+}
+
+simple_satellite_orbit() {
+  let ORBIT=10
+  let ORBIT=ORBIT*ORBIT
+  let ORBIT=$ORBIT+1
+}
+
+calculate_satellite_orbit() {
+  simple_satellite_orbit
+  echo "$ORBIT" > orbit.txt
+}
+
+calculate_flight_time() {
+  echo "0" > flight-time.txt
+}
+
+calculate_fuel() {
+  echo "0" > fuel.txt
+}
+
+calculate_accident_fall_point
+calculate_satellite_orbit
+calculate_flight_time
 calculate_fuel
